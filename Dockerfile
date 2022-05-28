3b26729a41e434524658e0a9da4508eb67d872f7
Burak Kara <burak@karasoftware.com>
2022-05-28 23:22:19 +0200

    First working version

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,30 @@
-FROM adamrehn/ue4-runtime:20.04-vulkan-noaudio
+#FROM adamrehn/ue4-runtime:20.04-vulkan-noaudio
+FROM adamrehn/ue4-runtime:20.04-cudagl11.4.2-noaudio
+
+# Already added in base image
+# ENV NVIDIA_DRIVER_CAPABILITIES ${NVIDIA_DRIVER_CAPABILITIES},video
 
 COPY CompiledProject /opt/pixel_streaming_bundle/CompiledProject
 COPY PixelStreamingServer /opt/pixel_streaming_bundle/PixelStreamingServer
 COPY RunEnv.sh /opt/pixel_streaming_bundle/RunEnv.sh
 
 USER root
+
+# NVIDIA keyrings are broken. These lines are for a temporary fix recommended in NVIDIA forums. NVIDIA fix starts.
+COPY cuda-keyring_1.0-1_all.deb /tmp/cuda-keyring_1.0-1_all.deb
+WORKDIR /tmp
+RUN sed -i '/developer\.download\.nvidia\.com\/compute\/cuda\/repos/d' /etc/apt/sources.list.d/*
+RUN sed -i '/developer\.download\.nvidia\.com\/compute\/machine-learning\/repos/d' /etc/apt/sources.list.d/*
+RUN apt-key del 7fa2af80 \ 
+    && dpkg -i cuda-keyring_1.0-1_all.deb
+# NVIDIA fix ends.
+
 RUN apt-get update -yq \
     && apt-get -yq install curl gnupg ca-certificates \
     && curl -L https://deb.nodesource.com/setup_12.x | bash \
     && apt-get update -yq \
-    && apt-get install -yq \
-        nodejs
+    && apt-get install -yq nodejs \
+    && apt-get install -yq coturn
 
 # Install the dependencies for the signalling server
 WORKDIR /opt/pixel_streaming_bundle/PixelStreamingServer
@@ -21,17 +35,11 @@ USER ue4
 # Expose TCP port 8080 for player WebSocket connections and web server HTTP access
 EXPOSE 8080
 
-# Expose TCP port 8888 for streamer WebSocket connections
-# No needed since we are embedding both here
-#EXPOSE 8888
-#EXPOSE 8888/udp
-
 # Google stun
 EXPOSE 19302
 
 # Turn coturn
-EXPOSE 3478
-EXPOSE 3479
+EXPOSE 19303
 
 # Set the packaged project as the container's entrypoint
-ENTRYPOINT ["/opt/pixel_streaming_bundle/RunEnv.sh", "-AudioMixer", "-PixelStreamingIP=localhost", "-PixelStreamingPort=8888", "-RenderOffScreen"]
+ENTRYPOINT ["/opt/pixel_streaming_bundle/RunEnv.sh", "-PixelStreamingURL=ws://localhost:8888", "-RenderOffScreen", "-UseHyperThreading", "-ResX=1920", "-ResY=1080", "-Windowed", "-ForceRes", "-Unattended"]
--- RunEnv.sh
+++ RunEnv.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
+# TURN setup starts
+publicip=$(curl -s http://api.ipify.org)
+localip=$(hostname -I | awk '{print $1}')
+turnport="19303"
+turnserveraddress="${publicip}:${turnport}"
+turnusername="PixelStreamingUser"
+turnpassword="AnotherTURNintheroad"
+realm="PixelStreaming"
+arguments="-p ${turnport} -r $realm -X $publicip -E $localip -L $localip --no-cli --no-tls --no-dtls --pidfile /var/run/turnserver.pid -f -a -v -n -u ${turnusername}:${turnpassword}"
+turnserver $arguments & # Start the turn server (coturn)
+stunserver="stun.l.google.com:19302"
+peerconnectionoptions="{\"iceServers\":[{\"urls\":[\"stun:$stunserver\",\"turn:$turnserveraddress\"],\"username\":\"$turnusername\",\"credential\":\"$turnpassword\"}]}"
+# TURN setup ends
+
 cd /opt/pixel_streaming_bundle/PixelStreamingServer
-node cirrus &
+node cirrus --peerConnectionOptions=\"$peerconnectionoptions\" &
 
 cd /opt/pixel_streaming_bundle/CompiledProject
 
@@ -16,9 +30,23 @@ UE_TRUE_SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
 
 UE_PROJECT_ROOT=$(dirname "$UE_TRUE_SCRIPT_NAME")/CompiledProject
 
+UE_BINARIES_BASE="$UE_PROJECT_ROOT/$UE_PROJECT_NAME/Binaries/Linux"
+
 # Create a symbolic link to the path where libnvidia-encode.so.1 will be mounted, since Unreal seems to ignore LD_LIBRARY_PATH
-ln -s /usr/lib/x86_64-linux-gnu/libnvidia-encode.so.1 $UE_PROJECT_ROOT/$UE_PROJECT_NAME/Binaries/Linux/libnvidia-encode.so.1
+ln -s /usr/lib/x86_64-linux-gnu/libnvidia-encode.so.1 $UE_BINARIES_BASE/libnvidia-encode.so.1
 
-chmod +x "$UE_PROJECT_ROOT/$UE_PROJECT_NAME/Binaries/Linux/$UE_PROJECT_NAME-Linux-Shipping"
+UE_PROJECT_BINARY=""
+if test -f "$UE_BINARIES_BASE/$UE_PROJECT_NAME"; then
+    UE_PROJECT_BINARY="$UE_BINARIES_BASE/$UE_PROJECT_NAME"
+elif test -f "$UE_BINARIES_BASE/$UE_PROJECT_NAME-Linux-Shipping"; then
+    UE_PROJECT_BINARY="$UE_BINARIES_BASE/$UE_PROJECT_NAME-Linux-Shipping"
+elif test -f "$UE_BINARIES_BASE/$UE_PROJECT_NAME-Linux-Debug"; then
+    UE_PROJECT_BINARY="$UE_BINARIES_BASE/$UE_PROJECT_NAME-Linux-Debug"
+else
+    echo "Could not find any candidate for project binary."
+    exit 1
+fi
 
-"$UE_PROJECT_ROOT/$UE_PROJECT_NAME/Binaries/Linux/$UE_PROJECT_NAME-Linux-Shipping" $UE_PROJECT_NAME "$@"
+#chmod +x $UE_PROJECT_BINARY
+
+$UE_PROJECT_BINARY $UE_PROJECT_NAME "$@"
